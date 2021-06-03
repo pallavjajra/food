d495cfccd9d0cefffa2da524f14301e4a395d897
pallav jajra <pallavjajra@gmail.com>
2021-06-03 19:04:28 +0530

    Initial commit

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', function(){
+    const header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+function toggleMenu(){
+    const menutoggle = document.querySelector('.menutoggle');
+    const navigation = document.querySelector('.navigation');
+    menutoggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
